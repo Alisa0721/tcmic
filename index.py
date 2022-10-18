5cac7f4d4985652156bfc3e59cfd59cb9b21b053
Alisa0721 <113404845+Alisa0721@users.noreply.github.com>
2022-10-18 10:36:55 +0800

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def index():
     homepage = "<h1>丁鈺蘋Python+flask+Vercel網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=yuping>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單輸入實例</a><br><br>"
     homepage += "<a href=/about>鈺蘋簡介網頁</a><br>"
     return homepage
